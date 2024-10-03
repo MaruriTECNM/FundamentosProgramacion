004d346ee9543baa410f4d7ac9cdbe803fba6797
MaruriTECNM <240i0044@martineztorre.tecnm.mx>
2024-10-03 17:57:11 -0600

    Add files via upload

---
+++ Basico.py
@@ -0,0 +1,15 @@
+#Reporte de uso basico del lenguaje python
+#Tipos de datos
+
+Entero = 1#Numero entero
+Flotante = 0.1#Numero con punto flotante
+Literal = 'TecNM Martinez de la Torre'#Literal de cadena
+LiteralNum = 0x12b#299 en Hexadecimal
+Entero_Usuario=int(input("Introduce un numero: "))#Entrada de datos, asignacion de datos
+Suma = Entero + Entero_Usuario#Operador de suma
+print("Estos son los tipos de datos en python!")
+print(Entero,"Es un numero entero.")
+print(Flotante, "Es un numero flotante con decimal.")
+print(Literal)
+print(LiteralNum, "Es una literal hexadecimal")
+print(Suma,"Esto es el resultado de la suma de",Entero, "y", Entero_Usuario,".")
